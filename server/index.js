4278d87996ffb7b08a9160f312c9c9266b090332
asuluntas <aysunsulun@gmail.com>
2019-04-18 11:26:10 -0700

    Refactored proxy to render video service

--- server/index.js
+++ server/index.js
@@ -1,49 +1,54 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
 const proxy = require('http-proxy-middleware');
 const cors = require('cors');
 const app = express();
-
 const PORT = process.env.PORT || 3000;
 
+app.use(morgan('dev'));
 app.use(cors());
 // serve up static file
-app.use(express.static('public'));
-app.use('/*/styles.css', express.static('public/styles.css'));
-app.use('/*', express.static('public'));
+const staticPath = `${__dirname}/../public`;
+console.log(staticPath);
 
-//
+// app.use('/:id', express.static(staticPath));
+// app.use('/*/styles.css', express.static('public/styles.css'));
+// app.use('/*', express.static('public'));
+
 // app.use(express.static(__dirname + '/../client/dist'));
 // app.use('/*/styles.css', express.static(__dirname + '/../client/dist/styles.css'));
 // app.use('/*', express.static(__dirname + '/../client/dist'));
 
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}...`);
-});
+app.use('/fandangit/:id', express.static(path.join(__dirname, '/../public')));
 
+// app.use('/videos/:id', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
 
-
-// video carousel
+//video carousel
 const videoCarouselOptions = {
-  target: 'http://localhost:3333',
-  // changeOrigin: true
+  target: 'http://localhost:3001',
+  changeOrigin: true
 };
 const videoCarouselProxy = proxy(videoCarouselOptions);
-app.use('/associatedVideos', videoCarouselProxy);
+app.use('/videos', videoCarouselProxy);
 
 
-const castCrewOptions = {
-  target: 'http://localhost:2002',
-  // changeOrigin: true
-};
-const castCrewProxy = proxy(castCrewOptions);
-app.use('/actors', castCrewProxy);
+// const castCrewOptions = {
+//   target: 'http://localhost:2002',
+//   changeOrigin: true
+// };
+// const castCrewProxy = proxy(castCrewOptions);
+// app.use('/actors/:id', castCrewProxy);
 
 
-const movieInfoOptions = {
-  target: 'http://localhost:2000',
-  // changeOrigin: true
-};
-const movieInfoProxy = proxy(movieInfoOptions);
-app.use('/movies', movieInfoProxy);
+// const movieInfoOptions = {
+//   target: 'http://localhost:2000',
+//   changeOrigin: true,
+// };
+// const movieInfoProxy = proxy(movieInfoOptions);
+// app.use('/info/:id', movieInfoProxy);
+
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
