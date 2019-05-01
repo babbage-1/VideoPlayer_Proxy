5f133aaae335171c520272d78ee342a9d08204b5
asuluntas <aysunsulun@gmail.com>
2019-04-30 20:07:46 -0700

    Update rautes

--- server/index.js
+++ server/index.js
@@ -9,46 +9,38 @@ const PORT = process.env.PORT || 3000;
 
 app.use(morgan('dev'));
 app.use(cors());
-// serve up static file
 const staticPath = `${__dirname}/../public`;
 console.log(staticPath);
 
-// app.use('/:id', express.static(staticPath));
-// app.use('/*/styles.css', express.static('public/styles.css'));
-// app.use('/*', express.static('public'));
-
-// app.use(express.static(__dirname + '/../client/dist'));
-// app.use('/*/styles.css', express.static(__dirname + '/../client/dist/styles.css'));
-// app.use('/*', express.static(__dirname + '/../client/dist'));
 
 
 app.use('/fandangit/:id', express.static(path.join(__dirname, '/../public')));
 
-// app.use('/videos/:id', proxy({ target: 'http://localhost:3001', changeOrigin: true }));
 
 //video carousel
 const videoCarouselOptions = {
-  target: 'http://localhost:3001',
+  target: 'http://ec2-54-67-84-44.us-west-1.compute.amazonaws.com:3001',
   changeOrigin: true
 };
 const videoCarouselProxy = proxy(videoCarouselOptions);
 app.use('/videos', videoCarouselProxy);
 
+//actors
+const actorsOptions = {
+  target: 'http://ec2-54-215-226-106.us-west-1.compute.amazonaws.com:2002',
+  changeOrigin: true
+};
+const actorsProxy = proxy(actorsOptions);
+app.use('/actors', actorsProxy);
 
-// const castCrewOptions = {
-//   target: 'http://localhost:2002',
-//   changeOrigin: true
-// };
-// const castCrewProxy = proxy(castCrewOptions);
-// app.use('/actors/:id', castCrewProxy);
+//main info
+const infoOptions = {
+  target: 'http://ec2-54-91-248-31.compute-1.amazonaws.com:2000',
+  changeOrigin: true
+};
+const infoProxy = proxy(infoOptions);
+app.use('/main', infoProxy);
 
-
-// const movieInfoOptions = {
-//   target: 'http://localhost:2000',
-//   changeOrigin: true,
-// };
-// const movieInfoProxy = proxy(movieInfoOptions);
-// app.use('/info/:id', movieInfoProxy);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
